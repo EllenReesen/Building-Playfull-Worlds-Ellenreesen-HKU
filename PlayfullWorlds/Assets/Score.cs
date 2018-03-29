cdfa6744aa557a46e660e82e0859e8c719f3a617
Ellen <ellen@reesen.net>
2018-03-29 21:10:06 +0200

    bpwhku

---
+++ PlayfullWorlds/Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour 
+{
+	public int score;
+
+	void Update()
+	{
+		GetComponent<Text> ().text = "Score: " + score;
+	}
+
+	//Voeg score toe vanuit andere scripts
+	public void addScore(int i)
+	{
+		score += i;
+	}
+}
---
+++ PlayfullWorlds/Assets/ShootingGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingGun : MonoBehaviour {
+
+	public GameObject kogel, kogelpunt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && !transform.parent.GetComponent<CoconutPickUp>().heeftCoconutVast()) 
+		{
+			GameObject schietkogel = Instantiate (kogel, kogelpunt.transform.position, Quaternion.Euler(kogelpunt.transform.eulerAngles.x, kogelpunt.transform.eulerAngles.y - 90, kogelpunt.transform.eulerAngles.z));
+			schietkogel.GetComponent<Rigidbody> ().AddForce (transform.parent.transform.forward * 2000);
+			transform.GetComponent<AudioSource> ().Play ();
+
+		}
+	}
+}
